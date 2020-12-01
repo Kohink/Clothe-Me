bc6bdd718aa4c34f0b72b740b33ceccd4824e282
gjrocky <gjb17@my.fsu.edu>
2020-11-30 22:14:22 -0500

    here

--- clotheme/clotheme/urls.py
+++ clotheme/clotheme/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login', include('login.urls')),
+    url(r'^login/', include('login.urls')),
 
 ]
