e19751b61a0cab6c5783d9130f7402604dcab238
mizai125 <74692357+mizai125@users.noreply.github.com>
2020-11-30 22:52:00 -0600

    user closet

--- clotheme/user_closet/models.py
+++ clotheme/user_closet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Closet(models.Model):
+    name = models.CharField(max_length=200)
+
+class Clothes(models.Model):
+    closet = models.ForeignKey(Closet, on_delete=models.CASCADE)
+    id = models.CharField(max_length=30)
+    type_cloth = models.CharField(max_length=200)
+    size = models.CharField(max_length=200)
