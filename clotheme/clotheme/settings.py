c12c81979602a4249aa9c87a2b95c7995d556cf4
gjrocky <gjb17@my.fsu.edu>
2020-11-30 22:48:03 -0500

    this is the User home page

--- clotheme/clotheme/settings.py
+++ clotheme/clotheme/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login',
+    'user_closet',
+    'user_profile',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- clotheme/login/views.py
+++ clotheme/login/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("<h1> Login Page")
+    return HttpResponse("templates/login.html")
 
 
