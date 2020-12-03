e76cdd30667d597f53a9d93d0588c8fee827e211
gjrocky <gjb17@my.fsu.edu>
2020-12-03 14:53:57 -0500

    need to push in order to pull

--- clotheme/user_closet/admin.py
+++ clotheme/user_closet/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Closet
 
-# Register your models here.
+admin.site.register(Closet)
--- clotheme/user_closet/models.py
+++ clotheme/user_closet/models.py
@@ -1,12 +1,15 @@
 from django.db import models
-
 # Create your models here.
 
 class Closet(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 class Clothes(models.Model):
     closet = models.ForeignKey(Closet, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
+    clothing_name = models.CharField(max_length=30)
     type_cloth = models.CharField(max_length=200)
     size = models.CharField(max_length=200)
+    image = models.CharField(max_length=4000)
