33d883985d4d230454d0783b533cbe4215d6cabd
mizai125 <74692357+mizai125@users.noreply.github.com>
2020-12-03 17:43:39 -0600

    html template

--- clotheme/user_closet/views.py
+++ clotheme/user_closet/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Closet
 
 def indexx(request):
     all_closets = Closet.objects.all()
-    html = ''
-    for closet in all_closets:
-        url = '/closet/' + str(closet.id) + '/'
-        html += '<a href="' + url + '">' + closet.name + '</a><br>'
-    return HttpResponse(html)
+    context = {'all_closets': all_closets}
+    return render(request, 'user_closet/index.html', context)
 
 def detail(request, closet_id):
     return HttpResponse("<h2>This is the User Closet: " + str(closet_id) + "</h2>")
