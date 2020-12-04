2313a49554d8ddcd3d938261fb577b8aa1559ae7
mizai125 <74692357+mizai125@users.noreply.github.com>
2020-12-03 22:06:46 -0600

    clothes

--- clotheme/user_closet/views.py
+++ clotheme/user_closet/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Closet
 
 def indexx(request):
     all_closets = Closet.objects.all()
-    context = {'all_closets': all_closets}
-    return render(request, 'user_closet/index.html', context)
+    return render(request, 'user_closet/index.html', {'all_closets': all_closets})
 
 def detail(request, closet_id):
-    return HttpResponse("<h2>This is the User Closet: " + str(closet_id) + "</h2>")
+    try:
+        closet = Closet.objects.get(pk=closet_id)
+    except Closet.DoesNotExist:
+        raise Http404("Closet does not exist")
+    return render(request, 'user_closet/detail.html', {'closet': closet})
