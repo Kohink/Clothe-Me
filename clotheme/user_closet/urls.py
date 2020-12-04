550a12a2a13a76b604ecf4bd1ddcf966a5d06eca
mizai125 <74692357+mizai125@users.noreply.github.com>
2020-12-04 06:08:13 -0600

    user registration

--- clotheme/user_closet/urls.py
+++ clotheme/user_closet/urls.py
@@ -5,6 +5,7 @@ app_name = 'user_closet'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('register/', views.UserFormView.as_view(), name='register'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('closet/add/', views.ClosetCreate.as_view(), name='closet-add'),
     path('closet/<int:pk>/', views.ClosetUpdate.as_view(), name='closet-update'),
--- clotheme/user_closet/views.py
+++ clotheme/user_closet/views.py
@@ -1,7 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from .models import Closet, Clothes
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'user_closet/index.html'
@@ -49,3 +53,34 @@ class ClothesCreate(CreateView):
         closet_number = self.kwargs['pk']
         form.instance.closet = Closet.objects.get(id = closet_number)
         return super(ClothesCreate, self).form_valid(form)
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'user_closet/registration_form.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+
+                if user.is_active:
+                    login(request, user)
+                    return redirect('/closet/')
+
+        return render(request, self.template_name, {'form': form})
